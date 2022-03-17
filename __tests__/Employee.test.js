41be177ceccad37d66b7e7f041987c5c123ff6a9
gpmike31 <gpmike3105@gmail.com>
2022-03-17 14:07:50 -0500

    created testing files for emplyoee and employee roles

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,11 +1,55 @@
-//Employee function to check if employee input will return employee stats and role
-const { expect } = require('@jest/globals');
-const Employee = require('../lib/Employee')
+const Employee = require("../lib/Employee.js");
 
-//test to expect function Employee to be truthy when given Employee inputs as an argument
-//to then display employee properties
-test('employee class properties', () => {
-    const employee = new Employee('Gerald', '1', 'gerald360@gmail.com');
+test("create an employee object", () => {
+  const employee = new Employee("gerald","1", "gp123@tester.com");
 
-    expect(employee.name).toBe('Gerald');
+  expect(employee.name).toBe("gerald");
+  expect(employee.id).toBe("1");
+  expect(employee.email).toBe("gerald123@tester.com")
 });
+
+// test("gets enemy's health value", () => {
+//   const enemy = new Enemy("goblin", "sword");
+
+//   expect(enemy.getHealth()).toEqual(
+//     expect.stringContaining(enemy.health.toString())
+//   );
+// });
+
+// test("gets a description of the enemy", () => {
+//   const enemy = new Enemy("goblin", "sword");
+
+//   expect(enemy.getDescription()).toEqual(expect.stringContaining("goblin"));
+//   expect(enemy.getDescription()).toEqual(expect.stringContaining("sword"));
+// });
+
+// test("checks if enemy is alive or not", () => {
+//   const enemy = new Enemy("goblin", "sword");
+
+//   expect(enemy.isAlive()).toBeTruthy();
+
+//   enemy.health = 0;
+
+//   expect(enemy.isAlive()).toBeFalsy();
+// });
+
+// test("gets enemy's attack value", () => {
+//   const enemy = new Enemy("goblin", "sword");
+//   enemy.strength = 10;
+
+//   expect(enemy.getAttackValue()).toBeGreaterThanOrEqual(5);
+//   expect(enemy.getAttackValue()).toBeLessThanOrEqual(15);
+// });
+
+// test("subtracts from enemy's health", () => {
+//   const enemy = new Enemy("goblin", "sword");
+//   const oldHealth = enemy.health;
+
+//   enemy.reduceHealth(5);
+
+//   expect(enemy.health).toBe(oldHealth - 5);
+
+//   enemy.reduceHealth(99999);
+
+//   expect(enemy.health).toBe(0);
+// });
