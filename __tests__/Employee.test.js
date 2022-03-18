082d26eb52d80431f20eb240360f40dae2512964
gpmike31 <gpmike3105@gmail.com>
2022-03-18 10:36:05 -0500

    edited employee test and object errors, created successful manager test and objects with office numbers added

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,11 +1,12 @@
 const Employee = require("../lib/Employee.js");
 
 test("create an employee object", () => {
-  const employee = new Employee("gerald","1", "gerald123@tester.com");
+  const employee = new Employee("Gerald","2", "gerald123@tester.com", "Employee");
 
-  expect(employee.name).toBe("gerald");
-  expect(employee.id).toBe("1");
-  expect(employee.email).toBe("gerald123@tester.com")
+  expect(employee.name).toBe("Gerald");
+  expect(employee.id).toBe("2");
+  expect(employee.email).toBe("gerald123@tester.com");
+  expect(employee.role).toBe("Employee");
 });
 
 // test("gets enemy's health value", () => {
